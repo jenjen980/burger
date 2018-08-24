673bf6a0d179a74f3dcaeef56068719e9088defd
Jenn <jswan02@gmail.com>
2018-08-24 09:44:51 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,22 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
+  // jawsDB
+  var connection;
+  if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+      connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    //local host
+      connection = mysql.createConnection({
+          root: 3000,
+          host: "localhost",
+          user: "root",
+          password: "",
+          database: "db_name",
+      });
+  };
+  
 // Make connection.
 connection.connect(function(err) {
   if (err) {
